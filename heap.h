78a0805808e4933c7fefdf4cd1a933d4f10c80fc
Tanuj Reddy Thummala <tthummala3672@sdsu.edu>
2025-10-31 21:41:06 -0700

    Implement pop() and downheap()

--- heap.h
+++ heap.h
@@ -15,18 +15,38 @@ struct MinHeap {
     MinHeap() { size = 0; }
 
     void push(int idx, int weightArr[]) {
+        // insert index at the end of heap
         data[size] = idx;
+
+        // restore order using upheap
         upheap(size, weightArr);
+
+        // increment size by 1 to ensure that the next push() and pop() operations perform correctly
         size++;
     }
 
     int pop(int weightArr[]) {
-        // TODO: remove and return smallest index
-        // Replace root with last element, then call downheap()
-        return -1; // placeholder
+        // if heap is empty, then return -1
+        if (size == 0) return -1;
+
+        int smallestIndex = data[0];
+
+        // replace root with last element
+        data[0] = data[size - 1];
+
+        data[size - 1] = -1;
+
+        // decrement size by 1 to ensure that the next push() and pop() operations perform correctly
+        size--;
+
+        // ensure node with the least value is at the root using downheap
+        downheap(0, weightArr);
+
+        return smallestIndex;
     }
 
     void upheap(int pos, int weightArr[]) {
+        // child node is not updated every loop because its value is constant
         int childNode = weightArr[data[pos]];
 
         // while pos is not the root
@@ -53,7 +73,55 @@ struct MinHeap {
     }
 
     void downheap(int pos, int weightArr[]) {
-        // TODO: swap parent downward while larger than any child
+        // parent node is not updated every loop because its value is constant
+        int parentNode = weightArr[data[pos]];
+
+        int leftChildIndex = (2 * pos) + 1;
+        int rightChildIndex = (2 * pos) + 2;
+
+        // while pos is not the last node of the heap and parent node has at least a left child
+        while (pos < size && leftChildIndex < size) {
+            int leftChildNode = weightArr[data[leftChildIndex]];
+
+            int smallerChildIndex;
+            int smallerChildNode;
+
+            // if right child index is not out of bounds
+            if (rightChildIndex < size) {
+                int rightChildNode = weightArr[data[rightChildIndex]];
+
+                // compare the two children nodes to see which is the smallest
+                smallerChildIndex = leftChildNode < rightChildNode ? leftChildIndex : rightChildIndex;
+                smallerChildNode = leftChildNode < rightChildNode ? leftChildNode : rightChildNode;
+            }
+            else {
+                // if right child index is out of bounds, then the only child is the left child
+                smallerChildIndex = leftChildIndex;
+                smallerChildNode = leftChildNode;
+            }
+
+            int temp = 0;
+
+            // compare parent node to its smallest child
+            if (parentNode > smallerChildNode) {
+                // if parent node is greater than its smallest child, then swap
+                temp = data[pos];
+                data[pos] = data[smallerChildIndex];
+                data[smallerChildIndex] = temp;
+
+                // the smallest child's index becomes the new pos (i.e. pos of parent after the swap = smallerChildIndex)
+                pos = smallerChildIndex;
+
+                // recompute left and right child indexes for the next iteration of the loop
+                leftChildIndex = (2 * pos) + 1;
+                rightChildIndex = (2 * pos) + 2;
+
+                continue;
+            }
+
+            // if the parent node is less than its smallest node, then end the loop
+            break;
+        }
     }
 };
 
